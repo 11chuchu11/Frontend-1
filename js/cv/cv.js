85abd0d78f0b7fce9f0f2e6afb70a8158fa447b6
franco <francoveronperalta@gmail.com>
2021-08-26 01:41:33 -0300

    agrego los archivos que me olvide en el anterior commit

---
+++ js/cv/cv.js
@@ -0,0 +1,10 @@
+// alert('tocá sobre mi foto para ir a la segunda pagina o sobre "chuchu" para ir a la pagina principal')
+Swal.fire({
+    text: 'tocá sobre mi foto para ir a la segunda pagina o sobre "chuchu" para ir a la pagina principal',
+    // background: '#000',
+    toast: true,
+    position: 'center',
+    // customClass: {
+    //     popup: 'pop-up'
+    // }
+})
---
+++ js/cv/sobremi.js
@@ -0,0 +1,11 @@
+
+
+Swal.fire({
+    text: 'tocá sobre mi foto para ir al cv o sobre "chuchu" para ir a la pagina principal',
+    // background: '#000',
+    toast: true,
+    position: 'center',
+    // customClass: {
+    //     popup: 'pop-up'
+    // }
+})
